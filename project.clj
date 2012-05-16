607149a7c6d944d5e848d075dcf90bfb621354b7
Zhao Shenyang <zhaosy@fastmail.fm>
2012-05-16 21:49:04 +0800

    add sms and ivr api support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hoiio-clj "1.0.0-SNAPSHOT"
+(defproject hoiio-clj "0.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-beanutils/commons-beanutils "1.8.2"]
--- src/hoiio_clj/client.clj
+++ src/hoiio_clj/client.clj
@@ -8,3 +8,52 @@
 
 (def hoiio-client
   (memoize hoiio-client*))
+
+(defn send-sms
+  "send a sms"
+  [cred dest msg & {:keys [sender-name, tag, notify-url]}]
+  (.smsSend
+   (hoiio-client cred)
+   dest sender-name msg tag notify-url))
+
+(defn ivr-dial
+  "make a outgoing IVR call"
+  [cred dest & {:keys [msg, caller-id, tag, notify-url]}]
+  (.ivrDial
+   (hoiio-client cred)
+   msg, dest, caller-id, tag, notify-url))
+
+(defn ivr-play
+  "IVR play the message"
+  [cred session & {:keys [msg tag notify-url]}]
+  (.ivrPlay
+   (hoiio-client cred)
+   session msg tag notify-url))
+
+(defn ivr-gather
+  "IVR gather"
+  [cred session notify-url & {:keys [msg max-digits timeout attempts tag]}]
+  (.ivrGather
+   (hoiio-client cred)
+   session msg max-digits timeout attempts tag notify-url))
+
+(defn ivr-record
+  "IVR Record"
+  [cred session notify-url & {:keys [msg max-duration tag]}]
+  (.ivrRecord
+   (hoiio-client cred)
+   session msg max-duration tag notify-url))
+
+(defn ivr-transfer
+  "IVR Transfer"
+  [cred session dest & {:keys [msg caller-id tag notify-url]}]
+  (.ivrTransfer
+   (hoiio-client cred)
+   session msg dest caller-id tag notify-url))
+
+(defn ivr-hangup
+  "IVR Hangup"
+  [cred session & {:keys [msg tag notify-url]}]
+  (.ivrHangup
+   (hoiio-client cred)
+   session msg tag notify-url))
